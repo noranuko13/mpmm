d05db67b3e1510b34d17abb7f7ada76bb80c4ee5
noranuko13 <noranuko13@gmail.com>
2023-05-10 00:32:31 +0900

    :notes: [mpmm] Add missing comment

--- src/scripts/sketch/myaku-list/myaku-list-lifo-iterator.ts
+++ src/scripts/sketch/myaku-list/myaku-list-lifo-iterator.ts
@@ -22,10 +22,18 @@ export class LifoMyakuListIterator implements IMyakuListIterator {
     this.next = this.myakuList.length;
   }
 
+  /**
+   * 次の要素が存在するか
+   * @returns 存在するときtrue,存在しないときfalse
+   */
   hasNext(): boolean {
     return 0 < this.next;
   }
 
+  /**
+   * 次の要素を取得する
+   * @returns 次のミャク
+   */
   getNext(): IMyaku {
     this.next--;
     return this.myakuList.get(this.next);
